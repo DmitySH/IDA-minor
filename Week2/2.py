0b836015788996af8a44f8f47ac71ed8eef33d6d
Дмитрий Шагаров <dashagarov@edu.hse.ru>
2021-09-14 23:29:22 +0300

    First week done

---
+++ Week2/2.py
@@ -0,0 +1,12 @@
+#    _~_
+#   (o o)
+#  /  V  \
+# /(  _  )\
+#   ^^ ^^
+
+n = int(input())
+print('   _~_    '*n, sep='')
+print('  (o o)   '*n, sep='')
+print(' /  V  \\  '*n, sep='')
+print('/(  _  )\\ '*n, sep='')
+print('  ^^ ^^   '*n, sep='')
