9dc3f515bed4a5003b8aaf33e07550f1ba4032d2
Дмитрий Шагаров <dashagarov@edu.hse.ru>
2021-09-29 09:40:17 +0300

    X added

--- Week_4/T.py
+++ Week_4/T.py
@@ -1,15 +1,7 @@
-from math import sqrt
-
-
-def cycle(n, had_sqr):
+def cycle(n):
     if n != 0:
-        cycle(int(input()), had_sqr)
-    if n > 0:
-        if (sqrt(n).is_integer()):
-            print(n, end=' ')
-            had_sqr = True
-        return had_sqr
+        cycle(int(input()))
+    print(n)
 
 
-if not cycle(int(input()), False):
-    print(0)
+cycle(int(input()))
--- Week_4/W.py
+++
@@ -1,5 +0,0 @@
-s = input()
-res = ''
-for sym in s:
-    res += sym + '*'
-print(res[:len(res) - 1])
